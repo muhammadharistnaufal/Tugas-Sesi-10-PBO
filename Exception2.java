14c4706541812262f06019f114f17aeb36f08710
muhammadharistnaufal <114551807+muhammadharistnaufal@users.noreply.github.com>
2024-06-04 12:20:30 +0700

    Add files via upload

---
+++ Exception2.java
@@ -0,0 +1,28 @@
+/**
+ *
+ * @author M Harist Naufal
+ */
+public class Exception2 {
+    public static void main(String [] args) {
+        int i=10;
+        String greetings[]= {
+            "Hello World!",
+            "NO, I mean it!",
+            "HELLO WORLD!"
+        };
+        while(i<4)
+        {
+            try
+            {
+                System.out.println(greetings[i]);
+                i++;
+            }
+            catch (ArrayIndexOutOfBoundsException e)
+            {
+                System.out.println("Resetting index value");
+                i=0;
+            }
+        }
+    }
+    
+}
